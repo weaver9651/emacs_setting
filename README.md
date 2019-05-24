dac6bc9aee5f40756e8a965b5653b9e8fcdac3ef
weaver9651 <weaver9651@naver.com>
2019-05-25 08:54:05 +0900

    meghanada(java mode) 주석 처리 및 elpy(python mode) 추가

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # emacs_setting
+## Included features
+- lint
+  - llvm-mode
+  - yaml-mode
+- neo-tree
+  - f8
+- xcscope
+- redo
+- hide menu bar
+- window dedication
+- reverse window
+- marmalate and melpa package link
+- magit(manually install)
+- VisualStudio-like debugging key setting
+  - f5 : continue
+  - f10 : next
+  - f11 : step
--- setup.sh
+++ setup.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-pushd $HOME/.emacs.d
-mkdir 
-git clone https://github.com/Fanael/rainbow-delimiters
-cd rainbow-delimiters
-cp rainbow-delimiters.el $HOME/.emacs.d/
+LISPDIR=$HOME/.emacs.d/lisp
+
+if [ -d $LISPDIR ]; then
+    echo "'$LISPDIR' exists."
+    pushd ./lisp
+    for ELFILE in *
+    do
+	isExist=$HOME/.emacs.d/lisp/$ELFILE
+	if [ -f $isExist ]; then
+	    continue
+	else
+	    cp $ELFILE $isExist
+	fi
+    done
+    popd
+else
+    echo "'$LISPDIR' does not exist."
+    mkdir -p ~/.emacs.d/lisp
+    pushd ./lisp
+    cp * $LISPDIR/
+    popd
+fi 
+
+
+if [ -f $HOME/.emacs ]; then
+    echo "'.emacs' exists."
+    cat ./emacs_config >> $HOME/.emacs
+else
+    echo "'.emacs' does not exist."
+    cp ./emacs_config $HOME/.emacs
+    echo "cp .emacs ~/.emacs"
+fi
+
+echo "To install magit, choose package link in .eamcs file then\n M-x package-refresh-contents RET\n M-x package-install RET magit RET\n"
+echo "To install meghanada, set melpa then M-x package-refresh-contents RET\n M-x package-install RET meghanada RET\n"
