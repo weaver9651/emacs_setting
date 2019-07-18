4affa94b477e7551078cf2636ccd499c2873394f
weaver9651 <weaver9651@naver.com>
2019-07-18 13:52:54 +0900

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+lispdir='$HOME/.emacs.d/lisp'
+
+if [ -d $lispdir ]; then
+    echo "'$lispdir' exists."
+else
+    mkdir -p $HOME/.emacs.d/lisp
+fi 
+
+pushd ./lisp
+cp * $lispdir
+popd
+
+if [ -f $HOME/.emacs ]; then
+    echo "'.emacs' exists."
+    cat ./emacs_config >> $HOME/.emacs
+else
+    cp ./emacs_config $HOME/.emacs
