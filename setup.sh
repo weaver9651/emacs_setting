d1e7cbb515059c4e4a519b499ff6812b05339797
weaver9651 <weaver9651@gmail.com>
2019-07-18 14:13:30 +0900

    modify setup.sh and add xcscope.el

--- setup.sh
+++ setup.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-lispdir='$HOME/.emacs.d/lisp'
+LISPDIR=$HOME/.emacs.d/lisp
 
-if [ -d $lispdir ]; then
-    echo "'$lispdir' exists."
+if [ -d $LISPDIR ]; then
+    echo "'$LISPDIR' exists."
+    pushd ./lisp
+    for ELFILE in *
+    do
+	isExist=$HOME/.emacs.d/lisp/$ELFILE
+	if [ -f $isExist ]; then
+	    continue
+	else
+	    cp $ELFILE $isExist
+	fi
+    done
+    popd
 else
-    mkdir -p $HOME/.emacs.d/lisp
+    echo "'$LISPDIR' does not exist."
+    mkdir -p ~/.emacs.d/lisp
+    pushd ./lisp
+    cp * $LISPDIR/
+    popd
 fi 
 
-pushd ./lisp
-cp * $lispdir
-popd
 
 if [ -f $HOME/.emacs ]; then
     echo "'.emacs' exists."
     cat ./emacs_config >> $HOME/.emacs
 else
+    echo "'.emacs' does not exist."
     cp ./emacs_config $HOME/.emacs
+fi
