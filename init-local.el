2a2826dbcd3390e2705e1cb5d669ac64159ba7b9
weaver9651 <weaver9651@gmail.com>
2025-05-07 15:10:14 -0400

    add git diff highlighter, clipboard copy & paste, and footer

--- init-local.el
+++ init-local.el
@@ -1,3 +1,7 @@
+;;; init-local.el --- my init -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 ;; set el files load path
 (setq load-path (cons (expand-file-name "~/.emacs.d/lisp") load-path))
 
@@ -61,6 +65,39 @@
 ;; show buffer list in current window
 (global-set-key (kbd "C-x b") 'buffer-menu)
 (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
+
+;; copy to clipboard ;; only works for window mode not terminal mode
+(setq x-select-enable-clipboard t)
+
+;; copy to clipboard
+(defun copy-to-clipboard ()
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+        (message "Yanked region to x-clipboard!")
+        (call-interactively 'clipboard-kill-ring-save)
+        )
+    (if (region-active-p)
+        (progn
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          (message "Yanked region to clipboard!")
+          (deactivate-mark))
+      (message "No region active; can't yank to clipboard!")))
+  )
+
+(defun paste-from-clipboard ()
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+        (clipboard-yank)
+        (message "graphics active")
+        )
+    (insert (shell-command-to-string "xsel -o -b"))
+    )
+  )
+
+;; (global-set-key [f8] 'copy-to-clipboard)
+;; (global-set-key [f9] 'paste-from-clipboard)
 
 ;;;;
 
@@ -198,6 +235,9 @@
   :ensure t)
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; highlight git diff
+(diff-hl-margin-mode)
+
 ;;;; python
 ;; pyright
 ;;(use-package lsp-pyright
@@ -207,5 +247,6 @@
 ;;                          (require 'lsp-pyright)
 ;;                          (lsp))))
 
-
 (provide 'init-local)
+
+;;; init-local.el ends here
